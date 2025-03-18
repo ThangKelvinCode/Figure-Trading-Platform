2c52df65736280d86a921396d5f57ec755f82eb3
HuyPP94 <huyppse184013@fpt.edu.vn>
2025-03-18 13:46:10 +0700

    Added Basic accessory page

--- client/src/pages/accessory_management.jsx
+++ client/src/pages/accessory_management.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const accessory_management = () => {
-  return <div>accessory_management</div>;
+  return (
+    <div>
+      <div className="header">
+        <h1>Accessory Management</h1>
+        <div className="button-group">
+          <button className="add_button">
+            <span>+</span> New Accessory
+          </button>
+        </div>
+      </div>
+      <div className="user_manage_page">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Image</th>
+              <th className="button">Action</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default accessory_management;
