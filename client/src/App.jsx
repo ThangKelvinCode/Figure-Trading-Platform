d91c30d5d91fa27bfe79beca843f96d9129da49d
FugitiveFusso <dodinhan1973@gmail.com>
2025-03-19 11:13:17 +0700

    fix bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Contact from './components/pages/Contact';
 import Home from "./components/pages/Home";
 import { Services } from './components/pages/Services';
 import PaymentSuccess from "./components/pages/PaymentSucess";
+import PaymentProcessing from "./components/pages/PaymentProcessing";
+import PaymentFailure from "./components/pages/PaymentFailure";
 function App() {
 
   return<div className="App">
@@ -16,8 +18,9 @@ function App() {
     <Route path="/about" element={<About/>}/>
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/services" element={<Services />}/>
-      <Route path="/paymentsucess" element={<PaymentSuccess />} />
-
+      <Route path="/payment-success" element={<PaymentSuccess />} />
+      <Route path="/payment-failure" element={<PaymentFailure />} />
+      <Route path="/payment-process" element={<PaymentProcessing />} />
     </Routes>   
     </div>
   
