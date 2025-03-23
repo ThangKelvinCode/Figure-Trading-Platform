f7d37cbd985c4df70be81937080754786a0fcc11
Mai Ly <lymai.09032004@gmail.com>
2025-03-23 16:35:05 +0700

    done review + review swagger doc

--- server/src/controllers/reviews.controller.js
+++ server/src/controllers/reviews.controller.js
@@ -2,9 +2,9 @@ import HTTP_STATUS from "../constants/httpStatus.js"
 import { REVIEW_MESSAGE } from "../constants/messages.js"
 import { reviewService } from "../services/reviews.services.js"
 import { ORDER_MESSAGE } from "../constants/messages.js"
+
 const writeReview = async (req, res) => {
     try {
-        console.log('con', req.params.detailID)
         const newReview = await reviewService.writeReview(req.body, req.params.id, req.params.detailID)
         if(newReview ==  null){
             return res.status(HTTP_STATUS.BAD_REQUEST).json({
@@ -31,9 +31,9 @@ const getReview = async(req, res) => {
 
 const editReview = async(req, res) => {
     try {
-        const { _id, content, star } = req.body || {}
-
-        const result = await reviewService.editReview(star, content, _id)
+        const userID = req.params.id
+        const reviewID = req.params.reviewID
+        const result = await reviewService.editReview(req.body, userID, reviewID)
         return res.status(HTTP_STATUS.OK).json({
             message: REVIEW_MESSAGE.UPDATE_SUCCESS,
             result
@@ -45,14 +45,17 @@ const editReview = async(req, res) => {
 
 const deleteReview = async(req, res) => {
     try {
-        const { userID } = req.body
-        const del = reviewService.deleteReview(userID, req.parmas.id)
+        const userID = req.params.id
+        const reviewID = req.params.reviewID
+        const del = await reviewService.deleteReview(userID, reviewID)
+
         return res.status(HTTP_STATUS.OK).json({
             message: REVIEW_MESSAGE.DELETE_SUCCESS,
             del
         })
+
     } catch (error) {
-        return res.status(HTTP_STATUS.NOT_FOUND).json({error: error.message})
+        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: error.message})
     }
 }
 
--- server/src/services/reviews.services.js
+++ server/src/services/reviews.services.js
@@ -6,20 +6,20 @@ import ordersRepo from "../repositories/orders.repo.js";
 import reviewRepo from "../repositories/reviews.repo.js";
 import userRepo from "../repositories/users.repo.js";
 import order_details from "../models/schemas/Order_Details.schema.js";
-import orders from "../models/schemas/Orders.schema.js";   
+import orders from "../models/schemas/Orders.schema.js";
 import { ObjectId } from "mongodb";
+import { usersServices } from "./users.services.js";
+
 const writeReview = async (reqBody, userID, detailID) => {
     try {
         if (!await userRepo.findById(userID)) {
             throw new Error(ORDER_MESSAGE.NO_PERMISSION)
         }
         const orderDetail = new order_details(await orderDetailsRepo.getOrderDetails(detailID))
-        console.log(orderDetail) //done
+
         const order = new orders(await ordersRepo.getOrder(orderDetail.order))
-        // console.log(userID) //done
-        console.log(order.status)
-        console.log(order._id)
-        if(order.status !== ORDER_STATUS.COMPLETED){
+
+        if (order.status !== ORDER_STATUS.COMPLETED) {
             return null
         }
         const newdata = {
@@ -42,18 +42,40 @@ const getReview = async (id) => {
     }
 }
 
-const editReview = async (star, content, id) => {
-    if (!await getReview(id)) {
-        throw new Error(ORDER_MESSAGE.NOT_FOUND)
+const editReview = async (reqBody, userID, reviewID) => {
+    try {
+        const review = new reviews(await getReview(reviewID))
+        if (!review) {
+            throw new Error(ORDER_MESSAGE.NOT_FOUND)
+        }
+    
+        if (userID != review.reviewer) { //check if the edit person and reviewer is the same person
+            throw new Error(ORDER_MESSAGE.NO_PERMISSION)
+        }
+    
+        const { star, content} = reqBody
+    
+        return await reviewRepo.editReview(star, content, reviewID)
+    } catch (error) {
+        throw new Error(error.message)
     }
-    return await reviewRepo.editReview(star, content, id)
 }
 
 const deleteReview = async (userID, reviewID) => {
-    if (await userRepo.findById(userID)) {
+    try {
+        const review = new reviews(await getReview(reviewID))
+        console.log(review)// k phai tai nay
+        if (!review) {
+            throw new Error(ORDER_MESSAGE.NOT_FOUND)
+        }
+        if (userID == await review.reviewer || await usersServices.checkAdmin(userID)) {
+            //check if the delete person and reviewer is the same person or must be admin
+            return await reviewRepo.deleteReview(reviewID)
+        }
         throw new Error(ORDER_MESSAGE.NO_PERMISSION)
+    } catch (error) {
+        throw new Error(error.message)
     }
-    return await reviewRepo.deleteReview(reviewID)
 }
 
 export const reviewService = {
