f334528b8228929034e15b37f8a2550972e86c8a
anh95488@gmail.com <anh95488@gmail.com>
2025-03-25 20:41:39 +0700

    format Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -80,23 +80,25 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../assets/css/Login.css";
-import { useAuth } from "../context/auth.jsx";function Login() {
+import { useAuth } from "../context/auth.jsx";
+function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const { login } = useAuth();
-  const navigate = useNavigate();  const handleSubmit = async (e) => {
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
 
-const loginSuccess = await login(email, password);
-if (loginSuccess) {
-  navigate("/");
-} else {
-  alert("Wrong email or password");
-}
-
-  };  return (
+    const loginSuccess = await login(email, password);
+    if (loginSuccess) {
+      navigate("/");
+    } else {
+      alert("Wrong email or password");
+    }
+  };
+  return (
     <div className="login_window">
       <div className="login_form">
         <h2>Sign in your account</h2>
@@ -121,5 +123,5 @@ if (loginSuccess) {
       </div>
     </div>
   );
-}export default Login;
-
+}
+export default Login;
