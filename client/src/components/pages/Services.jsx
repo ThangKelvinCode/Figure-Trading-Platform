7e486a99150573f1980418120ef57f1f97ecbe74
Mai Ly <lymai.09032004@gmail.com>
2025-04-22 00:42:55 +0700

    fix bug + gop back va front cho ae gan CV :D

---
+++ client/src/components/pages/Services.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export const Services = () => {
+    return <div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sit maiores laborum, illum reprehenderit doloremque facilis ullam nostrum corrupti, inventore consequatur esse exercitationem dignissimos fugit est asperiores itaque, vero aliquam quo voluptate officiis ad. Nostrum ullam error nihil eligendi repellendus dicta, vel numquam consectetur officiis omnis alias eum aliquam quam deleniti velit a praesentium consequatur sequi fugit nam sint facere voluptates? Quas, laboriosam? Itaque accusamus reiciendis possimus adipisci repellat blanditiis molestiae, quibusdam nihil ex, impedit at odit consequuntur, consequatur ab? Adipisci dolore, animi soluta reiciendis hic ab inventore consequatur, molestias aliquid a autem, velit alias corporis deserunt earum at expedita?
+
+        </div>
+
+        <br/>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sit maiores laborum, illum reprehenderit doloremque facilis ullam nostrum corrupti, inventore consequatur esse exercitationem dignissimos fugit est asperiores itaque, vero aliquam quo voluptate officiis ad. Nostrum ullam error nihil eligendi repellendus dicta, vel numquam consectetur officiis omnis alias eum aliquam quam deleniti velit a praesentium consequatur sequi fugit nam sint facere voluptates? Quas, laboriosam? Itaque accusamus reiciendis possimus adipisci repellat blanditiis molestiae, quibusdam nihil ex, impedit at odit consequuntur, consequatur ab? Adipisci dolore, animi soluta reiciendis hic ab inventore consequatur, molestias aliquid a autem, velit alias corporis deserunt earum at expedita?
+
+        </div>
+        <br/>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sit maiores laborum, illum reprehenderit doloremque facilis ullam nostrum corrupti, inventore consequatur esse exercitationem dignissimos fugit est asperiores itaque, vero aliquam quo voluptate officiis ad. Nostrum ullam error nihil eligendi repellendus dicta, vel numquam consectetur officiis omnis alias eum aliquam quam deleniti velit a praesentium consequatur sequi fugit nam sint facere voluptates? Quas, laboriosam? Itaque accusamus reiciendis possimus adipisci repellat blanditiis molestiae, quibusdam nihil ex, impedit at odit consequuntur, consequatur ab? Adipisci dolore, animi soluta reiciendis hic ab inventore consequatur, molestias aliquid a autem, velit alias corporis deserunt earum at expedita?
+
+        </div>
+        <br/>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sit maiores laborum, illum reprehenderit doloremque facilis ullam nostrum corrupti, inventore consequatur esse exercitationem dignissimos fugit est asperiores itaque, vero aliquam quo voluptate officiis ad. Nostrum ullam error nihil eligendi repellendus dicta, vel numquam consectetur officiis omnis alias eum aliquam quam deleniti velit a praesentium consequatur sequi fugit nam sint facere voluptates? Quas, laboriosam? Itaque accusamus reiciendis possimus adipisci repellat blanditiis molestiae, quibusdam nihil ex, impedit at odit consequuntur, consequatur ab? Adipisci dolore, animi soluta reiciendis hic ab inventore consequatur, molestias aliquid a autem, velit alias corporis deserunt earum at expedita?
+
+        </div>
+        <br/>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sit maiores laborum, illum reprehenderit doloremque facilis ullam nostrum corrupti, inventore consequatur esse exercitationem dignissimos fugit est asperiores itaque, vero aliquam quo voluptate officiis ad. Nostrum ullam error nihil eligendi repellendus dicta, vel numquam consectetur officiis omnis alias eum aliquam quam deleniti velit a praesentium consequatur sequi fugit nam sint facere voluptates? Quas, laboriosam? Itaque accusamus reiciendis possimus adipisci repellat blanditiis molestiae, quibusdam nihil ex, impedit at odit consequuntur, consequatur ab? Adipisci dolore, animi soluta reiciendis hic ab inventore consequatur, molestias aliquid a autem, velit alias corporis deserunt earum at expedita?
+
+        </div>
+        <br/>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sit maiores laborum, illum reprehenderit doloremque facilis ullam nostrum corrupti, inventore consequatur esse exercitationem dignissimos fugit est asperiores itaque, vero aliquam quo voluptate officiis ad. Nostrum ullam error nihil eligendi repellendus dicta, vel numquam consectetur officiis omnis alias eum aliquam quam deleniti velit a praesentium consequatur sequi fugit nam sint facere voluptates? Quas, laboriosam? Itaque accusamus reiciendis possimus adipisci repellat blanditiis molestiae, quibusdam nihil ex, impedit at odit consequuntur, consequatur ab? Adipisci dolore, animi soluta reiciendis hic ab inventore consequatur, molestias aliquid a autem, velit alias corporis deserunt earum at expedita?
+
+        </div>
+        <br/>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sit maiores laborum, illum reprehenderit doloremque facilis ullam nostrum corrupti, inventore consequatur esse exercitationem dignissimos fugit est asperiores itaque, vero aliquam quo voluptate officiis ad. Nostrum ullam error nihil eligendi repellendus dicta, vel numquam consectetur officiis omnis alias eum aliquam quam deleniti velit a praesentium consequatur sequi fugit nam sint facere voluptates? Quas, laboriosam? Itaque accusamus reiciendis possimus adipisci repellat blanditiis molestiae, quibusdam nihil ex, impedit at odit consequuntur, consequatur ab? Adipisci dolore, animi soluta reiciendis hic ab inventore consequatur, molestias aliquid a autem, velit alias corporis deserunt earum at expedita?
+
+        </div>
+        <br/>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sit maiores laborum, illum reprehenderit doloremque facilis ullam nostrum corrupti, inventore consequatur esse exercitationem dignissimos fugit est asperiores itaque, vero aliquam quo voluptate officiis ad. Nostrum ullam error nihil eligendi repellendus dicta, vel numquam consectetur officiis omnis alias eum aliquam quam deleniti velit a praesentium consequatur sequi fugit nam sint facere voluptates? Quas, laboriosam? Itaque accusamus reiciendis possimus adipisci repellat blanditiis molestiae, quibusdam nihil ex, impedit at odit consequuntur, consequatur ab? Adipisci dolore, animi soluta reiciendis hic ab inventore consequatur, molestias aliquid a autem, velit alias corporis deserunt earum at expedita?
+
+        </div>
+        <br/>
+    </div>;
+
+};
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
   server: { port: 5174 },
-=======
-  server: {
-    port: 5174 // change here
-  },
->>>>>>> 99b36a28d54bc624eec445cf368163ddd3b43e29
 });
